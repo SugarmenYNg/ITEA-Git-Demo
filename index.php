d7abb4cf198fc15c50e5e35a914fd94579f9191a
yuriy tsukrenko <sugarmenyn@gmail.com>
2018-06-13 18:40:44 +0300

    Add Composer Pakeges strstr zend-uri

--- index.php
+++ index.php
@@ -5,5 +5,15 @@
  * Date: 11.06.18
  * Time: 20:10
  */
+require __DIR__. "/vendor/autoload.php";
+
+$str = new \Str\Str("This is test string");
+
+$isContains = $str->contains("test");
+var_dump($isContains);
+
+$isContains = $str->contains("not exists");
+var_dump($isContains);
+
 //просто коментарий
-echo "Initial commit";
+//echo "Initial commit";
